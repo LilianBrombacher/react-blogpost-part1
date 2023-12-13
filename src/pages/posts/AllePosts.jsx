c1c613b497848f6fd05de2a572771747f422c824
LilianBrombacher <lbrombacher91@gmail.com>
2023-12-13 14:31:54 +0100

    posttitels weergeven op de postspagina

--- src/pages/posts/AllePosts.jsx
+++ src/pages/posts/AllePosts.jsx
@@ -1,10 +1,20 @@
 import posts from '../../constants/data.json';
+import './AllePosts.css'
+
 function AllePosts () {
     console.log(posts)
     return (
         <>
-        <main>
+        <main className='outer-container'>
             <h1>Alle posts</h1>
+            <ul className='posts'>
+                {posts.map((post) => (
+                    <li className='posts-short' key={post.id}>
+                        <p>{post.title} ({post.author})</p>
+                        <p>{post.comments} reacties  -  {post.shares} keer gedeeld</p>
+                    </li>
+                ))}
+            </ul>
         </main>
         </>
     )
