fd78806ded6401e5e5f264ae955cd7372a9b8b97
LilianBrombacher <lbrombacher91@gmail.com>
2023-12-19 14:00:35 +0100

    formulier nieuwe post pagina gemaakt

--- src/pages/newblog/NieuwePost.jsx
+++ src/pages/newblog/NieuwePost.jsx
@@ -1,11 +1,89 @@
-
-
-
+import NieuwePost from './NieuwePost.css'
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import calculateReadTime from "../../helpers/calculateReadTime.js";
 
 function nieuwePost () {
+    const [formState, setFormState]= useState({
+        title: '',
+        subtitle: '',
+        author: '',
+        content: '',
+    });
+
+    const navigate= useNavigate()
+
+    function handleChange(e) {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log({
+            ...formState,
+            shares: 0,
+            comments: 0,
+            created: new Date().toISOString(),
+            readTime: calculateReadTime(formState.content),
+        });
+
+        console.log('De blog is succesvol verzameld! 🌈');
+        // navigate('/posts');
+    }
+
+
+
     return (
         <main>
-            <h1>Nieuwe post maken</h1>
+
+            <form className="new-post-form" onSubmit={handleSubmit}>
+                <h1>Post toevoegen</h1>
+                <label htmlFor="post-title">Titel</label>
+                <input
+                    type="text"
+                    id="post-title"
+                    name="title"
+                    required
+                    value={formState.title}
+                    onChange={handleChange}
+                />
+                <label htmlFor="post-subtitle">Subtitle</label>
+                <input
+                    type="text"
+                    id="post-subtitle"
+                    name="subtitle"
+                    required
+                    value={formState.subtitle}
+                    onChange={handleChange}
+                />
+                <label htmlFor="post-author">Naam en achternaam</label>
+                <input
+                    type="text"
+                    id="post-author"
+                    name="author"
+                    required
+                    value={formState.author}
+                    onChange={handleChange}
+                />
+                <label htmlFor="post-content">Blogpost</label>
+                <textarea
+                    name="content"
+                    id="post-content"
+                    cols="30"
+                    rows="10"
+                    required
+                    minLength={300}
+                    maxLength={2000}
+                    value={formState.content}
+                    onChange={handleChange}></textarea>
+                <button type="submit">
+                    Toevoegen
+                </button>
+            </form>
         </main>
     )
 }
--- src/pages/posts/BlogPostDetail.jsx
+++ src/pages/posts/BlogPostDetail.jsx
@@ -1,41 +1,47 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import data from "../../constants/data.json"
+import posts from "../../constants/data.json"
 import {Link} from "react-router-dom";
+import formatDateString from "../../helpers/FormatDateString.jsx";
 
 
 function blogPostDetail() {
     const {postId} = useParams();
     const [postData, setPostData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                console.log(data)
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             console.log(data)
+    //
+    //             const postIdNumber = Number(postId);
+    //
+    //             const post = data.find(post => post.id === postIdNumber);
+    //
+    //             if (post) {
+    //                 setPostData(post);
+    //             } else {
+    //                 console.warn(`Blog post with id ${postIdNumber} not found`);
+    //             }
+    //             } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //             }
+    //     };
+    // fetchData();
+    // }, [postId]);
 
-                const postIdNumber = Number(postId);
-
-                const post = data.find(post => post.id === postIdNumber);
-
-                if (post) {
-                    setPostData(post);
-                } else {
-                    console.warn(`Blog post with id ${postIdNumber} not found`);
-                }
-                } catch (error) {
-                console.error('Error fetching data:', error);
-                }
-        };
-    fetchData();
-    }, [postId]);
+    const {title, readTime, subtitle, author, created, content, comments, shares} = posts.find((post) => {
+        return post.id.toString() === postId;
+    });
 
     return (
         <div>
-            <h1>{postData ? postData.title : 'Er is geen blog gevonden'}</h1>
-            <h2>{postData ? postData.subtitle : ""}</h2>
-            <p>Geschreven door {postData ? postData.author : ""} op {postData ? postData.created : ''}</p>
-            <p>{postData ? postData.content : 'Klik hier om terug te gaan naar de overzichtspagina'}</p>
-            <p>{postData ? postData.comments : ''} reacties {postData ? postData.shares: ''} - keer gedeeld</p>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+            <p>Geschreven door {author} op {formatDateString(created)}</p>
+            <p>{readTime} minuten lezen</p>
+            <p>{content}</p>
+            <p>{comments} reacties {shares} - keer gedeeld</p>
             <p> <Link className='to-all-posts' to='/alleposts'>Terug naar de overzichtspagina</Link> </p>
 
         </div>
